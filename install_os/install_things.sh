b3070fd1b0cb24dd6726e3f874eed89435dcb7f4
Konrad Gotfryd <kgotfryd.4l@gmail.com>
2018-07-16 21:08:58 +0200

    Fix few things

--- install_os/install_things.sh
+++ install_os/install_things.sh
@@ -2,4 +2,5 @@
 
 cat ../bash/bashrc >> ~/.bashrc
 rm -f ~/.vimrc
-ln -s ../vim/vimrc ~/.vimrc
+ln -s /home/kgotfryd/programming/workspace/ordinary_backup/vim/vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
